edfdfdf854abbb1a988cc69dbdb684c79eeacd5b
Aleksandr Zorin <aleksandr-zoryn@ya.ru>
2023-11-17 21:43:57 +0300

    ssr

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import useSWR from 'swr';
 import styles from '../components/Text.module.css';
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
-export default function Index() {
-  const {data, error, isLoading} = useSWR('/api/user', fetcher);
-
-  if (error) return <div className={styles.text}>ошибка загрузки</div>;
-  if (isLoading) return <div className={styles.text}>загрузка...</div>;
+export default function Index({data}) {
   return <div className={styles.text}>привет, {data.name}!</div>;
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://master--creative-nasturtium-9bf5e9.netlify.app/api/user`);
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return {props: {data}};
 }
