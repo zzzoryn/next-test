ec1bd9653e5c2f363734d0a3b5507e387fcf1022
Aleksandr Zorin <aleksandr-zoryn@ya.ru>
2023-11-17 21:30:22 +0300

    styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import useSWR from 'swr';
+import styles from '../components/Text.module.css';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
   const {data, error, isLoading} = useSWR('/api/user', fetcher);
 
-  if (error) return <div>ошибка загрузки</div>;
-  if (isLoading) return <div>загрузка...</div>;
-  return <div>привет, {data.name}!</div>;
+  if (error) return <div className={styles.text}>ошибка загрузки</div>;
+  if (isLoading) return <div className={styles.text}>загрузка...</div>;
+  return <div className={styles.text}>привет, {data.name}!</div>;
 }
